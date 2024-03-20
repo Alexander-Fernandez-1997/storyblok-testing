eee725ad1cf4c61cf8c42275af68dae7cf03790b
alexander fernandez <alex97l@hotmail.com>
2024-03-20 16:18:02 -0300

    no json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,54 +6,50 @@ export const Footer = async () => {
   const { logo, politic, contact, news, social_media, footer_links } =
     data.story.content;
   return (
-    <>
-      <code>{JSON.stringify(data.story.content, null, 2)}</code>
-
-      <footer className="pt-2 pb-1">
-        <div className="layout-container">
-          <div className="d-flex justify-content-between">
-            <div className="d-flex gap-1 flex-column">
-              <img
-                className="newsletter-logo"
-                src={logo.filename}
-                alt={logo.id}
-              />
-              <div>
-                <p className="text-bold mb-0 mt-0">{contact[0].street}</p>
-                <p className="text-bold mb-0 mt-0">{contact[0].number}</p>
-                <p className="text-bold mb-0 mt-0">Fax: {contact[0].fax}</p>
-              </div>
-              <div className="d-flex gap-1">
-                {social_media.map((social) => (
-                  <img
-                    className="social-media"
-                    src={social.icon.url}
-                    alt={social.icon.id}
-                  />
-                ))}
-              </div>
-            </div>
+    <footer className="pt-2 pb-1">
+      <div className="layout-container">
+        <div className="d-flex justify-content-between">
+          <div className="d-flex gap-1 flex-column">
+            <img
+              className="newsletter-logo"
+              src={logo.filename}
+              alt={logo.id}
+            />
             <div>
-              <h2>{news[0].tittle}</h2>
-              <p className="text-bold">{news[0].message}</p>
-              <button className="text-bold p-1 newsletter-btn text-white">
-                {news[0].btn_text}
-              </button>
+              <p className="text-bold mb-0 mt-0">{contact[0].street}</p>
+              <p className="text-bold mb-0 mt-0">{contact[0].number}</p>
+              <p className="text-bold mb-0 mt-0">Fax: {contact[0].fax}</p>
             </div>
-          </div>
-          <div className="d-flex justify-content-between border-top mt-2">
-            <div class="d-flex  gap-1 mt-1">
-              {footer_links.map((link) => (
-                <a href={link.href.url} className="text-bold">
-                  {link.text}
-                </a>
+            <div className="d-flex gap-1">
+              {social_media.map((social) => (
+                <img
+                  className="social-media"
+                  src={social.icon.url}
+                  alt={social.icon.id}
+                />
               ))}
             </div>
-            <p className="text-bold">{politic}</p>
+          </div>
+          <div>
+            <h2>{news[0].tittle}</h2>
+            <p className="text-bold">{news[0].message}</p>
+            <button className="text-bold p-1 newsletter-btn text-white">
+              {news[0].btn_text}
+            </button>
           </div>
         </div>
-      </footer>
-    </>
+        <div className="d-flex justify-content-between border-top mt-2">
+          <div class="d-flex  gap-1 mt-1">
+            {footer_links.map((link) => (
+              <a href={link.href.url} className="text-bold">
+                {link.text}
+              </a>
+            ))}
+          </div>
+          <p className="text-bold">{politic}</p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
